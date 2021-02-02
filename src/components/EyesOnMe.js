e93403feb9bdc6e9710d9b6368fa68d5fe312538
Blaire Baker <bakerblaire@gmail.com>
2021-02-01 20:49:01 -0600

    done

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,14 @@
-// Code EyesOnMe Component Here
+import React from 'react';
+
+class EyesOnMe extends React.Component {
+    render() {
+        return(
+            <button
+                onFocus={() => console.log('Good!')}
+                onBlur={() => console.log('Hey! Eyes on me!')}>
+            </button>
+        )
+    }
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,17 @@
-// Code Keypad Component Here
+import React from 'react';
+
+class Keypad extends React.Component {
+    render() {
+        return(
+            <form> 
+                <input 
+                    type="password"
+                    onKeyUp={() => console.log('Entering password...')}>
+                </input>
+            </form>
+            
+        )
+    }
+}
+
+export default Keypad;
